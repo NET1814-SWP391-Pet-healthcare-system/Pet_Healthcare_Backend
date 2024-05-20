ec0e1487e3ce2fca0ab820a5620cb897019712f3
Ung Nguyen Song Phuc <phucunsse183483@fpt.edu.vn>
2024-05-20 16:13:14 +0700

    Add RepositoryContracts: IPetRepository, IPetVaccinationRepository

---
+++ RepositoryContracts/IPetVaccinationRepository.cs
@@ -0,0 +1,19 @@
+﻿using PetHealthCareSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryContracts
+{
+    internal interface IPetVaccinationRepository
+    {
+        IEnumerable<PetVaccination> GetAll();
+        User GetById(int id);       //2 id which are primary key?
+        bool Add(PetVaccination petVaccination);
+        bool Update(PetVaccination petVaccination);
+        bool Remove(int id);    //2 id which are primary key?
+        bool SaveChanges();
+    }
+}
