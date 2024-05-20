a9816436de9ab492ea8ecc2d72a5bd9c786bc772
Ung Nguyen Song Phuc <phucunsse183483@fpt.edu.vn>
2024-05-20 17:58:52 +0700

    Add data annotation for: Pet, User, Vaccine

--- Entities/Pet.cs
+++ Entities/Pet.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Entities
 {
+    [Table("Pet")]
     public class Pet
     {
+        [Key]
         public int petId { get; set; }
+        [ForeignKey("User")]
         public User customerId { get; set; }
         public string name { get; set; } 
         public string species { get; set; }
