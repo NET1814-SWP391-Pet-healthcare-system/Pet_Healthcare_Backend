655219acc7a422f149620724a65ccbcd3d2dcb37
Ung Nguyen Song Phuc <phucunsse183483@fpt.edu.vn>
2024-05-20 11:33:10 +0700

    Add RepositoryContracts: IPetRepository, IUserRepository

---
+++ RepositoryContracts/IPetRepository.cs
@@ -0,0 +1,19 @@
+﻿using PetHealthCareSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts
+{
+    public interface IPetRepository
+    {
+        IEnumerable<Pet> GetAll();
+        User GetById(int id);
+        bool Add(Pet pet);
+        bool Update(Pet pet);
+        bool Remove(int id);
+        bool SaveChanges();
+    }
+}
