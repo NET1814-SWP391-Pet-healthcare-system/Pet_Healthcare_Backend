6dd263442e34c4490dd0253a1f04cc309270f5f8
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2024-05-20 17:54:40 +0700

    Hospitalization Interface and Repo

---
+++ Repositories/HospitalizationRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RepositoryContracts;
+using Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    internal class HospitalizationRepository : IHospitalizationRepository
+    {
+        bool IHospitalizationRepository.Add(Hospitalization hospitalization)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Hospitalization> IHospitalizationRepository.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Hospitalization IHospitalizationRepository.GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IHospitalizationRepository.Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IHospitalizationRepository.SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IHospitalizationRepository.Update(Hospitalization hospitalization)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
