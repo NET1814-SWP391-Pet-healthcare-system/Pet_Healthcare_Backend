65917735fd30d5d56b3f7aaf2c24a0926a7c12d1
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-05-20 16:46:57 +0700

    Add repository for Appointment, Slot, Service

---
+++ Repositories/AppointmentRepository.cs
@@ -0,0 +1,43 @@
+﻿using PetHealthCareSystem.Models;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        public bool Add(Appointment appointment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Appointment> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Appointment GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Appointment appointment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repositories/ServiceRepository.cs
@@ -0,0 +1,43 @@
+﻿using PetHealthCareSystem.Models;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        public bool Add(Service service)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Service> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Service GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Service service)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
