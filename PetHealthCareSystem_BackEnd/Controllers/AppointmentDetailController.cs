1454b14497c4fa46a63ce48ef8f9bb2fce0cb74a
Tan Nguyen <tannd1008@gmail.com>
2024-05-26 23:14:23 +0700

    Update Appointment Detail DTO, Service and Controller

--- PetHealthCareSystem_BackEnd/Controllers/AppointmentDetailController.cs
+++ PetHealthCareSystem_BackEnd/Controllers/AppointmentDetailController.cs
@@ -1,7 +1,10 @@
 ﻿using Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
 using ServiceContracts.DTO.AppointmentDetailDTO;
+using ServiceContracts.DTO.Result;
+using ServiceContracts.DTO.UserDTO;
 using Services;
 
 namespace PetHealthCareSystem_BackEnd.Controllers
@@ -13,35 +16,100 @@ namespace PetHealthCareSystem_BackEnd.Controllers
         private readonly ApplicationDbContext _context;
         private readonly AppointmentDetailService _appointmentDetailService;
 
-        public AppointmentDetailController(ApplicationDbContext context, AppointmentDetailService appointmentDetailService)
-        {
-            _context = context;
-            _appointmentDetailService = appointmentDetailService;
-        }
-
+        //Create
         [HttpPost]
-        public IActionResult AddAppointmentDetail(AppointmentDetailAddRequest? appointmentDetailAddRequest)
+        public ActionResult<BusinessResult> AddAppointmentDetail(AppointmentDetailAddRequest? appointmentDetailAddRequest)
         {
-            if(appointmentDetailAddRequest == null)
+            BusinessResult businessResult = new BusinessResult();
+            if (appointmentDetailAddRequest == null)
             {
-                return BadRequest("AppointmentDetailRequest is null");
+                businessResult.Status = 400;
+                businessResult.Data = null;
+                businessResult.Message = "AppointmentDetail request is null";
+                return BadRequest(businessResult);
             }
 
             _appointmentDetailService.AddAppointmentDetail(appointmentDetailAddRequest);
-
-            return Ok("Created successfully");
+            businessResult.Status = 404;
+            businessResult.Data = null;
+            businessResult.Message = "No AppointmentDetail found";
+            return Ok(businessResult);
         }
 
-        [HttpGet("{id}")]
-        public ActionResult<AppointmentDetail> GetAppointmentDetailById(int id)
+        //Read
+        [HttpGet]
+        public ActionResult<BusinessResult> GetAppointmentDetails()
         {
-            var appointmentDetail = _appointmentDetailService.GetAppointmentDetail(id);
-            if(appointmentDetail == null)
-            {
-                return BadRequest("AppointmentDetail not found");
-            }
-            return Ok(appointmentDetail);
+            BusinessResult businessResult = new BusinessResult();
+            businessResult.Data = _appointmentDetailService.GetAppointmentDetails();
+            businessResult.Message = "Successful";
+            businessResult.Status = 200;
+            return Ok(businessResult);
         }
 
+        [HttpGet("id/{id}")]
+        public ActionResult<BusinessResult> GetAppointmentDetailById(int id)
+        {
+            BusinessResult businessResult = new BusinessResult();
+            var user = _appointmentDetailService.GetAppointmentDetailById(id);
+            if (user == null)
+            {
+                businessResult.Status = 404;
+                businessResult.Data = null;
+                businessResult.Message = "No User found";
+                return NotFound(businessResult);
+            }
+            businessResult.Status = 200;
+            businessResult.Data = user;
+            businessResult.Message = "AppointmentDetail found";
+            return Ok(businessResult);
+        }
+
+
+        //Update
+        [HttpPut("{id}")]
+        public ActionResult<BusinessResult> UpdateAppointmentDetail(AppointmentDetailUpdateRequest? appointmentDetailUpdateRequest)
+        {
+            BusinessResult businessResult = new BusinessResult();
+            if (appointmentDetailUpdateRequest == null)
+            {
+                businessResult.Status = 400;
+                businessResult.Data = null;
+                businessResult.Message = "Request is null";
+                return BadRequest(businessResult);
+            }
+            var isUpdated = appointmentDetailUpdateRequest.UpdateAppointmentDetail(appointmentDetailUpdateRequest);
+            if (isUpdated == null)
+            {
+                businessResult.Status = 404;
+                businessResult.Data = null;
+                businessResult.Message = "AppointmentDetail not found";
+                return NotFound(businessResult);
+            }
+            businessResult.Status = 200;
+            businessResult.Data = appointmentDetailUpdateRequest.UpdateAppointmentDetail();
+            businessResult.Message = "User updated";
+            return Ok(businessResult);
+        }
+
+        //Delete
+        [HttpDelete("{username}")]
+        public ActionResult<BusinessResult> DeleteUserByUsername(int id)
+        {
+            BusinessResult businessResult = new BusinessResult();
+            var userData = _appointmentDetailService.GetAppointmentDetailById(id);
+            var isDeleted = _appointmentDetailService.RemoveAppointmentDetail(id);
+            if (!isDeleted)
+            {
+                businessResult.Status = 404;
+                businessResult.Data = null;
+                businessResult.Message = "AppointmentDetail not found";
+                return NotFound(businessResult);
+            }
+            businessResult.Status = 200;
+            businessResult.Data = userData;
+            businessResult.Message = "AppointmentDetail deleted";
+            return Ok(businessResult);
+        }
     }
 }
