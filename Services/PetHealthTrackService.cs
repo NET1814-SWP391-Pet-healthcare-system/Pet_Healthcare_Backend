7fccab1f6d9f4054b893f6732706a337621f9935
DangNgocThinhAn <thinhan1701@gmail.com>
2024-05-27 20:48:50 +0700

    Update PetHealthtrack controller and service

--- Services/PetHealthTrackService.cs
+++ Services/PetHealthTrackService.cs
@@ -13,29 +13,55 @@ namespace Services
     public class PetHealthTrackService : IPetHealthTrackService
     {
         private readonly IPetHealthTrackRepository _petHealthTrackRepository;
+        public PetHealthTrackService(IPetHealthTrackRepository petHealthTrackRepository)
+        {
+            _petHealthTrackRepository = petHealthTrackRepository;
+        }
+
         public bool AddPetHealthTrack(PetHealthTrackAddRequest request)
         {
-            throw new NotImplementedException();
+            if(request == null)
+            {
+                return false;
+            }
+            var pht = request.toPetHealthTrack();
+            
+            return _petHealthTrackRepository.Add(pht);
         }
 
         public PetHealthTrack? GetPetHealthTrackById(int id)
         {
-            throw new NotImplementedException();
+            return _petHealthTrackRepository.GetById(id);
         }
 
         public IEnumerable<PetHealthTrack> GetPetHealthTracks()
         {
-            throw new NotImplementedException();
+            return _petHealthTrackRepository.GetAll();
         }
 
         public bool RemovePetHealthTrack(int id)
         {
-            throw new NotImplementedException();
+            var pht = _petHealthTrackRepository.GetById(id);
+            if(pht == null)
+            {
+                return false;
+            }
+
+            return _petHealthTrackRepository.Remove(id);
         }
 
         public bool UpdatePetHealthTrack(PetHealthTrackUpdateRequest request)
         {
-            throw new NotImplementedException();
+            var existingPHT = _petHealthTrackRepository.GetById(request.PetHealthTrackId);
+            if(existingPHT == null) return false;
+            if (request == null) return false;
+            var phtModel = request.toPetHealthTrack();
+            existingPHT.HospitalizationId = phtModel.HospitalizationId;
+            existingPHT.Hospitalization = phtModel.Hospitalization;
+            existingPHT.Date = phtModel.Date;
+            existingPHT.Description = phtModel.Description;
+            existingPHT.Status = phtModel.Status;
+            return _petHealthTrackRepository.Update(existingPHT);
         }
     }
 }
