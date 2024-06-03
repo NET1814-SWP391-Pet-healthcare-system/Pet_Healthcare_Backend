047f2f825e660d1244b6d7e1cea3d04a27b85636
AugustanHalo <tannd1008@gmail.com>
2024-06-03 11:17:37 +0700

    Create RecordValidation.cs

---
+++ PetHealthCareSystem_BackEnd/Validations/RecordValidation.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using ServiceContracts;
+
+namespace PetHealthCareSystem_BackEnd.Validations
+{
+    public static class RecordValidation
+    {
+        private static readonly IRecordService _recordService;
+        private static readonly IPetService _petService;
+        public static bool IsRecordValid(Record record)
+        {
+            int petId = (int)record.PetId;
+            if(_petService.GetPetById(petId)==null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
