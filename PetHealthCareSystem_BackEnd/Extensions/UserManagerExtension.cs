a5722c67743bb0fe6369de3df6d08374dd35ef6f
Ung Nguyen Song Phuc <phucunsse183483@fpt.edu.vn>
2024-06-08 11:06:18 +0700

    Add AdminController feature
    Add AdminEmployee policy, Add GetUserDtosInRoleAsync() in UserManagerExtension, Add GetUserByRole(string role) in AdminController

--- PetHealthCareSystem_BackEnd/Extensions/UserManagerExtension.cs
+++ PetHealthCareSystem_BackEnd/Extensions/UserManagerExtension.cs
@@ -44,5 +44,81 @@ namespace PetHealthCareSystem_BackEnd.Extensions
             result.UserId = existingUser.Id;
             return result;
         }
+
+        public async static Task<IEnumerable<UserDTO>> GetUserDtosInRoleAsync(this UserManager<User> userManager,string role)
+        {
+            List<UserDTO> result = new List<UserDTO>();
+            switch(role.ToUpper())
+            {
+                case "CUSTOMER":
+                    var customerList = await userManager.GetUsersInRoleAsync("Customer");
+                    foreach(var user in customerList)
+                    {
+                        var customer = user as Customer;
+                        UserDTO userDTO = new UserDTO()
+                        {
+                            UserId = customer.Id,
+                            UserName = customer.UserName,
+                            FirstName = customer.FirstName,
+                            LastName = customer.LastName,
+                            Gender = customer.Gender,
+                            Address = customer.Address,
+                            Country = customer.Country,
+                            IsActive = customer.IsActive,
+                            Email = customer.Email,
+                            PhoneNumber = customer.PhoneNumber
+                        };
+                        result.Add(userDTO);
+                    }
+
+                    break;
+
+                case "VET":
+                    var vetList = await userManager.GetUsersInRoleAsync("Vet");
+                    foreach(var user in vetList)
+                    {
+                        var vet = user as Vet;
+                        UserDTO userDTO = new UserDTO()
+                        {
+                            UserId = vet.Id,
+                            UserName = vet.UserName,
+                            FirstName = vet.FirstName,
+                            LastName = vet.LastName,
+                            Gender = vet.Gender,
+                            Address = vet.Address,
+                            Country = vet.Country,
+                            IsActive = vet.IsActive,
+                            Email = vet.Email,
+                            PhoneNumber = vet.PhoneNumber,
+                            Rating = vet.Rating,
+                            YearsOfExperience = vet.YearsOfExperience
+                        };
+                        result.Add(userDTO);
+                    }
+                    break;
+
+                case "EMPLOYEE":
+                    var employeeList = await userManager.GetUsersInRoleAsync("Employee");
+                    foreach(var user in employeeList)
+                    {
+                        UserDTO userDTO = new UserDTO()
+                        {
+                            UserId = user.Id,
+                            UserName = user.UserName,
+                            FirstName = user.FirstName,
+                            LastName = user.LastName,
+                            Gender = user.Gender,
+                            Address = user.Address,
+                            Country = user.Country,
+                            IsActive = user.IsActive,
+                            Email = user.Email,
+                            PhoneNumber = user.PhoneNumber
+                        };
+                        result.Add(userDTO);
+                    }
+                    break;
+            }
+            return result;
+        }
     }
 }
