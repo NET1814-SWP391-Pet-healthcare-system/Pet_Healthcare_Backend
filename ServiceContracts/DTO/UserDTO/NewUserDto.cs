f72dfa48cae98524ee2285d081fb39cc267f85ba
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-06-08 16:28:10 +0700

    Add role to user login

--- ServiceContracts/DTO/UserDTO/NewUserDto.cs
+++ ServiceContracts/DTO/UserDTO/NewUserDto.cs
@@ -10,6 +10,7 @@ namespace ServiceContracts.DTO.UserDTO
     {
         public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
         public string Token { get; set; } = string.Empty;
     }
 }
