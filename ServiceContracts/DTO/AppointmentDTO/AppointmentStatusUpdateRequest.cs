52ec45f79be196884b7dd10705f5c4bd8a924cbe
Ung Nguyen Song Phuc <phucunsse183483@fpt.edu.vn>
2024-06-09 14:33:18 +0700

    Add UpdateAppointmentStatus
    Add UpdateAppointmentStatus for Admin and Employee Policy, when set status to cancel, CancellationDate is set to Utc.Now

---
+++ ServiceContracts/DTO/AppointmentDTO/AppointmentStatusUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO.AppointmentDTO
+{
+    public class AppointmentStatusUpdateRequest
+    {
+        public int Id { get; set; }
+        public AppointmentStatus Status { get; set; }
+    }
+}
