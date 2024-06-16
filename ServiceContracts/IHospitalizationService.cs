6f373199fc784a77fb9acbf3cabf3ec09f590730
Phạm Anh Kiệt <kietpase183731@fpt.edu.vn>
2024-06-16 15:46:36 +0700

    Rewrite Hospitalization to new Scheme Change HospitalizationGetByVetId to Get Add Validation for Vet time between each hospitalizations

--- ServiceContracts/IHospitalizationService.cs
+++ ServiceContracts/IHospitalizationService.cs
@@ -10,14 +10,14 @@ namespace ServiceContracts
 {
     public interface IHospitalizationService
     {
-        Task<Hospitalization> AddHospitalization(Hospitalization request);
-        Task<Hospitalization?> GetHospitalizationById(int id);
+        Task<HospitalizationDTO?> AddHospitalization(Hospitalization request);
+        Task<HospitalizationDTO?> GetHospitalizationById(int id);
         Task <IEnumerable<Hospitalization>> GetHospitalizations();
-        Task<Hospitalization?> UpdateHospitalization(int id, Hospitalization request);
-        Task<Hospitalization?> RemoveHospitalization(int id);
-        Task<Hospitalization?> GetHospitalizationByPetId(int id);
-        Task<List<Hospitalization>> GetAllHospitalizationByVetId(string id);
-        Task<List<Hospitalization>> GetAllHospitalizationByPetId(int id);
-        Task<bool> IsVetFree(Hospitalization hospitalization);
+        Task<HospitalizationDTO?> UpdateHospitalization(Hospitalization request);
+        Task<bool> RemoveHospitalization(int id);
+        Task<HospitalizationDTO?> GetHospitalizationByPetId(int id);
+        Task<IEnumerable<Hospitalization>> GetAllHospitalizationByVetId(string id);
+        Task<IEnumerable<Hospitalization>> GetAllHospitalizationByPetId(int id);
+        Task<bool> IsVetFree(string id, DateOnly AddmissionDate, DateOnly DischargeDate);
     }
 }
