1fc91360f0523a015e3bc83c5c460f15f18a6000
AugustanHalo <tannd1008@gmail.com>
2024-06-16 17:19:23 +0700

    Add prototype of refund

--- PetHealthCareSystem_BackEnd/Controllers/PaymentController.cs
+++ PetHealthCareSystem_BackEnd/Controllers/PaymentController.cs
@@ -23,11 +23,13 @@ namespace PetHealthCareSystem_BackEnd.Controllers
     {
         public IBrainTreeConfig _config;
         public IAppointmentService _appointmentService;
+        public UserManager<User> _userService;
 
-        public PaymentController(IBrainTreeConfig config, IAppointmentService appointmentService)
+        public PaymentController(IBrainTreeConfig config, IAppointmentService appointmentService, UserManager<User> userService)
         {
             _config = config;
             _appointmentService = appointmentService;
+            _userService = userService;
         }
 
         public static readonly TransactionStatus[] transactionSuccessStatuses =
@@ -133,5 +135,78 @@ namespace PetHealthCareSystem_BackEnd.Controllers
 
            
         }
+
+        //public async Task<IActionResult> Refund(int customerid, int appointmentid)
+        //{
+        //    BusinessResult businessResult = new BusinessResult();
+        //    string paymentStatus = string.Empty;
+        //    var gateway = _config.GetGateway();
+        //    if(!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    var appointment = await _appointmentService.GetAppointmentByIdAsync(appointmentid);
+        //    if (appointment == null)
+        //    {
+        //        businessResult.Status = 404;
+        //        businessResult.Data = null;
+        //        businessResult.Message = "Appointment Not Found";
+        //        return BadRequest(businessResult);
+        //    }
+        //    if(appointment.Status == AppointmentStatus.Done || appointment.Status == AppointStatus.Cancelled)
+        //    {
+        //        businessResult.Status = 404;
+        //        businessResult.Data = null;
+        //        businessResult.Message = "Appointment Already Paid";
+        //        return BadRequest(businessResult);
+        //    }
+
+
+        //    var customerName =  _userService.GetUserName(this.User);
+        //    var customer = await _userService.FindByNameAsync(customerName);
+        //    if (customer == null)
+        //    {
+        //        businessResult.Status = 404;
+        //        businessResult.Data = null;
+        //        businessResult.Message = "Customer Not Found";
+        //        return BadRequest(businessResult);
+        //    }
+
+        //    if(appointment.Customer!= customer)
+        //    {
+        //        businessResult.Status = 404;
+        //        businessResult.Data = null;
+        //        businessResult.Message = "Customer Not Owner Of This Transaction";
+        //        return BadRequest(businessResult);
+        //    }
+
+
+            // var transactionId = _InvoiceService.GetTransactionIdByAppointmentId(appointmentid);
+            // if (transactionId == null)
+            // {
+            //     businessResult.Status = 404;
+            //     businessResult.Data = null;
+            //     businessResult.Message = "Transaction Not Found";
+            //     return BadRequest(businessResult);
+            // }
+
+            // Result<Transaction> result = gateway.Transaction.Refund(transactionId);
+            // if (result.IsSuccess())
+            // {
+            //     paymentStatus = "Succeded";
+            //     //Do Database Operations Here
+            //}
+            // else
+            // {
+            //     string errorMessages = "";
+            //     foreach (ValidationError error in result.Errors.DeepAll())
+            //     {
+            //         errorMessages += "Error: " + (int)error.Code + " - " + error.Message + "\n";
+            //     }
+            //     paymentStatus = errorMessages;
+            // }
+
+        //}
     }
 }
