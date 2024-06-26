405b55600be09f04641c7566a128baec4649ccf8
Ung Nguyen Song Phuc <phucunsse183483@fpt.edu.vn>
2024-06-26 17:15:02 +0700

    Modify RegisterDto
    Add Password Confirmation and Error Messages

--- ServiceContracts/DTO/UserDTO/RegisterDto.cs
+++ ServiceContracts/DTO/UserDTO/RegisterDto.cs
@@ -12,13 +12,16 @@ namespace ServiceContracts.DTO.UserDTO
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public bool Gender { get; set; } = true;
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
         public string? Username { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
         [EmailAddress]
         public string? Email { get; set;}
-        [Required]
+        [Required(ErrorMessage = "Password is required")]
         public string? Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords must be matched")]
+        public string? ConfirmPassword { get; set; }
         public string Address { get; set; } = string.Empty;
         public string Country { get; set; } = string.Empty;
         public string ImageURL { get; set; } = string.Empty;
